389521b9ff4ad243f1e84e45999a64c2f951c6a9
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-06-30 11:56:34 +0530

    capability section integrated

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,9 @@ import styles from './home.module.css';
 import HomeHeaderComp from '@/component/home-page/header/header';
 import Image from 'next/image';
 import CheckIcon from '@mui/icons-material/Check';
+import QrCodeScannerIcon from '@mui/icons-material/QrCodeScanner';
+import HighQualityIcon from '@mui/icons-material/HighQuality';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 
 export default function HomePage() {
     return (
@@ -37,7 +40,6 @@ export default function HomePage() {
                 <Box className={styles.rightbusinessInfoBox}>
                     <Box className={styles.buisnessInfoImageBox}>
                         <Image src={'/home-page/rightBusinessInfo.jpg'} alt='rightBusinessInfo.jpg' width={100} height={100} className={styles.buisnessInfoImage} />
-
                     </Box>
 
                     <Box className={styles.buisnessInfoImageTagBox}>
@@ -52,7 +54,27 @@ export default function HomePage() {
             </Box>
 
             <Box className={styles.coreCapabilites}>
+                <Typography className={styles.coreCapabilitesTitle} >Core Capabilities</Typography>
 
+                <Box className={styles.capabilityContainer}>
+                    <Box className={styles.capabilityBox}>
+                        <QrCodeScannerIcon className={styles.capabilityBoxIconButton} />
+                        <Typography className={styles.capabilityTitle} >Instant QR Sharing</Typography>
+                        <Typography className={styles.capabilityDescription} >Generate elegant, customizable QR codes for instant event-wide photo access and engagement.</Typography>
+                    </Box>
+
+                    <Box className={styles.capabilityBox}>
+                        <HighQualityIcon className={styles.capabilityBoxIconButton} />
+                        <Typography className={styles.capabilityTitle} >Infinite Resolution</Typography>
+                        <Typography className={styles.capabilityDescription} >Preserve every pixel with our high-fidelity storage system designed for the most demanding visual artists.</Typography>
+                    </Box>
+
+                    <Box className={styles.capabilityBox}>
+                        <AutoAwesomeIcon className={styles.capabilityBoxIconButton} />
+                        <Typography className={styles.capabilityTitle} >Crowdsourced Magic</Typography>
+                        <Typography className={styles.capabilityDescription} >Effortlessly collect and curate moments from every attendee into a single, cohesive masterpiece.</Typography>
+                    </Box>
+                </Box>
             </Box>
         </Box>
     )
