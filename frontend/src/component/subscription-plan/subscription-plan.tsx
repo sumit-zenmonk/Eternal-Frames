f5ba1b02d728a1b6cb6e01419395efa272789ed0
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-07-01 13:10:52 +0530

    page content render nicely

--- frontend/src/component/subscription-plan/subscription-plan.tsx
+++ frontend/src/component/subscription-plan/subscription-plan.tsx
@@ -2,13 +2,15 @@
 
 import { useAppDispatch, useAppSelector } from "@/redux/hooks.ts";
 import { RootState } from "@/redux/store";
-import { Box, CircularProgress, Typography } from '@mui/material';
+import { Box, Card, CircularProgress, Typography } from '@mui/material';
 import styles from './subscription-plan.module.css';
 import { useEffect, useState } from "react";
 import { getSubscriptionPlan } from "@/redux/feature/subscription/subscription-action";
 import { enqueueSnackbar } from "notistack";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { SubscriptionPlan } from "@/redux/feature/subscription/subscription-type";
+import { Feature, SubscriptionPlan } from "@/redux/feature/subscription/subscription-type";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
 
 export default function SubscriptionPlanComp() {
     const dispatch = useAppDispatch();
@@ -45,16 +47,51 @@ export default function SubscriptionPlanComp() {
                     scrollableTarget="scrollableDiv"
                 >
                     <Box className={styles.planWrapper}>
-                        {subscriptionPlans.length && subscriptionPlans.map((plan: SubscriptionPlan) => {
+                        {subscriptionPlans.length && subscriptionPlans.map((plan: SubscriptionPlan, idx: number) => {
                             return (
-                                <Box key={plan.uuid}>
-                                    {plan.title}
-                                </Box>
+                                <Card
+                                    key={idx}
+                                    className={styles.card}
+                                    elevation={2}
+                                >
+                                    <Box className={styles.header}>
+                                        <Typography className={styles.title}>
+                                            {plan.title}
+                                        </Typography>
+
+                                        <Typography className={styles.description}>
+                                            {plan.description}
+                                        </Typography>
+                                    </Box>
+
+                                    <Box className={styles.pricing}>
+                                        <Typography className={styles.currency}>
+                                            {plan.currency}
+                                        </Typography>
+
+                                        <Typography className={styles.price}>
+                                            {plan.price}
+                                        </Typography>
+
+                                        <Typography className={styles.duration}>
+                                            /month
+                                        </Typography>
+                                    </Box>
+
+                                    {plan.features.length && plan.features.map((feature: Feature, idx: number) => (
+                                        <Box className={styles.featureBox} key={idx}>
+                                            {feature.is_included ? <CheckCircleIcon /> : <CheckCircleOutlinedIcon />}
+                                            <Typography className={styles.currency}>
+                                                {feature.feature_name}
+                                            </Typography>
+                                        </Box>
+                                    ))}
+                                </Card>
                             )
                         })}
                     </Box>
                 </InfiniteScroll>
-            </Box>
-        </Box>
+            </Box >
+        </Box >
     );
 }
