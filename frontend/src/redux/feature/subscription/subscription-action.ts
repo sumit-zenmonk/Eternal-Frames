4286f107e7806fb163fbba0aabea927b28aacf83
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-07-01 14:41:10 +0530

    redux api integrated

--- frontend/src/redux/feature/subscription/subscription-action.ts
+++ frontend/src/redux/feature/subscription/subscription-action.ts
@@ -45,3 +45,34 @@ export const getSubscriptionPlan = createAsyncThunk<
         }
     }
 );
+
+export const getRazorPayLinkForSubscription = createAsyncThunk<
+    any,
+    { total_price: number, plan_uuid: string },
+    { state: RootState }
+>(
+    "razor/pay/link",
+    async (payload, { getState, rejectWithValue }) => {
+        try {
+            const token = getState().authReducer.token || "";
+
+            const res = await fetch(`${BACKEND_URL}/api/v1/razor/pay/link`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: token,
+                },
+                body: JSON.stringify(payload),
+            });
+
+            const result = await res.json();
+            if (!res.ok) {
+                throw new Error(result.message);
+            }
+
+            return result;
+        } catch (error: any) {
+            return rejectWithValue(error.message);
+        }
+    }
+);
