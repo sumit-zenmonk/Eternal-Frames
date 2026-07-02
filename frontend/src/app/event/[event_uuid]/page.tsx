cc35d8d9e7a58704c6c4d558d201431df3874635
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-07-02 13:20:23 +0530

    ui visual shown

--- frontend/src/app/event/[event_uuid]/page.tsx
+++ frontend/src/app/event/[event_uuid]/page.tsx
@@ -1,14 +1,39 @@
-import { Box, } from '@mui/material';
+'use client'
+
+import { Box, Button, } from '@mui/material';
 import styles from './event.module.css';
 import HomeHeaderComp from '@/component/common/header/header';
+import { useAppSelector } from '@/redux/hooks.ts';
+import { RootState } from '@/redux/store';
+import { useParams } from 'next/navigation';
+import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 
 export default function HomePage() {
+    const { events } = useAppSelector((state: RootState) => state.eventReducer);
+    const { event_uuid } = useParams();
+    const event = events.find((eve) => eve.uuid === event_uuid);
+
     return (
         <Box className={styles.container}>
             <Box className={styles.headerBox}>
                 <HomeHeaderComp />
             </Box>
 
+            <Box className={styles.eventBody}>
+                <Box className={styles.eventInfo}>
+                    <Box className={styles.eventTitle}>
+                        {event?.title || 'N/A Title'}
+                    </Box>
+                    <Box className={styles.eventDescription}>
+                        {event?.description}
+                    </Box>
+                    <Button
+                        startIcon={<FileUploadOutlinedIcon />}
+                    >
+                        Upload New Image
+                    </Button>
+                </Box>
+            </Box>
         </Box>
     )
 }
