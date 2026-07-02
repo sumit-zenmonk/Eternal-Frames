9d3cc009d4ca23c14c06b88ee8da60527603ce5a
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-07-02 10:01:14 +0530

    css improved

--- frontend/src/component/gallery-sidebar/gallery-sidebar.tsx
+++ frontend/src/component/gallery-sidebar/gallery-sidebar.tsx
@@ -1,10 +1,33 @@
-import { Box, Typography } from '@mui/material';
+'use client'
+
+import { Box, Button, Typography } from '@mui/material';
 import styles from './gallery-sidebar.module.css';
 import EventIcon from '@mui/icons-material/Event';
 import AccountBoxOutlinedIcon from '@mui/icons-material/AccountBoxOutlined';
-import Link from 'next/link';
+import { useAppDispatch } from '@/redux/hooks.ts';
+import { logoutUser } from '@/redux/feature/auth/auth-action';
+import { enqueueSnackbar } from 'notistack';
+import { useRouter } from 'next/navigation';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export default function GallerySideBarComp() {
+    const router = useRouter()
+    const dispatch = useAppDispatch();
+
+    const handleLogOut = async () => {
+        try {
+            await dispatch(logoutUser()).unwrap();
+            localStorage.clear()
+            router.replace("/")
+        } catch (err: any) {
+            enqueueSnackbar(err, { variant: "error" })
+        }
+    }
+
+    const handleSwitchPages = async (path: string) => {
+        router.push(path)
+    }
+
     return (
         <Box className={styles.container}>
             <Box className={styles.header}>
@@ -18,13 +41,29 @@ export default function GallerySideBarComp() {
             </Box>
 
             <Box className={styles.tabBox}>
-                <Link href={'/gallery/event'}>
-                    <EventIcon />     Events
-                </Link>
+                <Button
+                    className={styles.button}
+                    onClick={async () => { await handleSwitchPages('/gallery/event') }}
+                >
+                    <EventIcon />Events
+                </Button>
 
-                <Link href={'/gallery/account'}>
-                    <AccountBoxOutlinedIcon />   Account
-                </Link>
+                <Button
+                    className={styles.button}
+                    onClick={async () => { await handleSwitchPages('/gallery/account') }}
+                >
+                    <AccountBoxOutlinedIcon />Account
+                </Button>
+            </Box>
+
+            <Box className={styles.footer}>
+                <Button
+                    className={styles.logoutbtn}
+                    onClick={async () => { await handleLogOut() }}
+                >
+                    <LogoutIcon className={styles.iconButton} />
+                    Log Out
+                </Button>
             </Box>
         </Box>
     );
