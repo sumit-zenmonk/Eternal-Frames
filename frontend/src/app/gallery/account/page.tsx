976836e2856a2c4bfe4f0281856ea95240fc4e04
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-07-02 16:54:41 +0530

    structure included

--- frontend/src/app/gallery/account/page.tsx
+++ frontend/src/app/gallery/account/page.tsx
@@ -6,7 +6,25 @@ import styles from './account.module.css';
 export default function GalleryAccountPage() {
     return (
         <Box className={styles.container}>
-            account page
+            <Box className={styles.topContainer}>
+                <Box className={styles.header}>
+                    <Typography className={styles.title}>Profile Management</Typography>
+                </Box>
+
+                <Box>
+
+                </Box>
+            </Box>
+
+            <Box className={styles.bottomContainer}>
+                <Box className={styles.header}>
+                    <Typography className={styles.title}>Subscription Management</Typography>
+                </Box>
+
+                <Box>
+
+                </Box>
+            </Box>
         </Box>
     );
 }
